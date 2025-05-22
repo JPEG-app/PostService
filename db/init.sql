9b670a5075d1e31d6b02d19ee569bfc2bf867864
Dimitar Georgiev <d.georgiev0348@gmail.com>
2025-05-22 20:38:50 +0200

    Likes db table

--- db/init.sql
+++ db/init.sql
@@ -12,3 +12,17 @@ CREATE TABLE IF NOT EXISTS cached_valid_users (
     created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE likes (
+    like_id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    user_id UUID NOT NULL,
+    post_id UUID NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_post_for_like
+        FOREIGN KEY(post_id)
+        REFERENCES posts(post_id)
+        ON DELETE CASCADE,
+    UNIQUE (user_id, post_id)
+);
+
+CREATE INDEX idx_likes_user_id ON likes(user_id);
